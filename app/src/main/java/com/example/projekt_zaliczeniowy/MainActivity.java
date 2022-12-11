93707a0d3a2f7b6c92e412c1e03846cf53ae08dd
Patryk Szymanski <patrykoss2004@gmail.com>
2022-12-11 03:46:55 +0100

    bottom navigation bar

--- app/src/main/java/com/example/projekt_zaliczeniowy/MainActivity.java
+++ app/src/main/java/com/example/projekt_zaliczeniowy/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.projekt_zaliczeniowy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.projekt_zaliczeniowy.fragments.AccountFragment;
+import com.example.projekt_zaliczeniowy.fragments.HomeFragment;
+import com.example.projekt_zaliczeniowy.fragments.ShoppingCartFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    HomeFragment homeFragment = new HomeFragment();
+    ShoppingCartFragment shoppingCartFragment = new ShoppingCartFragment();
+    AccountFragment accountFragment = new AccountFragment();
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // hide top bar
+        getSupportActionBar().hide();
+
+        // bottom navigation view setup
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+                return true;
+            case R.id.shoppingCart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, shoppingCartFragment).commit();
+                return true;
+            case R.id.account:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, accountFragment).commit();
+                return true;
+        }
+
+        return false;
     }
 }
